4b69ea68c3aef92e6260a25c2685c0d21491c16c
Igor Savin <iselwin@gmail.com>
2026-09-20 23:57:56 +0300

    Handle empty token vectors in cosine similarity (#74)
    * Handle empty token vectors in cosine similarity
    
    * Format repository

--- packages/similarity/src/cosine-similarity.ts
+++ packages/similarity/src/cosine-similarity.ts
@@ -120,11 +120,11 @@ class CosineSimilarity {
       return 1;
     }
 
-    const [termFreqVecA, termFreqVecB] = this.getTermFreqVectors(
-      strA,
-      strB,
-      locale
-    );
+    const vectors = this.getTermFreqVectors(strA, strB, locale);
+    if (vectors === 0) {
+      return 0;
+    }
+    const [termFreqVecA, termFreqVecB] = vectors;
     return this.cosineSimilarity(termFreqVecA, termFreqVecB);
   }
 }
