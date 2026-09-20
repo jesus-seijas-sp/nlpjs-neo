6fcba82fa094338c97f8a64ab966f0fa80ae3317
Igor Savin <iselwin@gmail.com>
2026-09-21 00:20:25 +0300

    Reset NLU intents after corpus preparation (#80)
    * Reset NLU intents after corpus preparation
    
    * Fix NLU cache regression assertion
    
    * Format NLU cache regression test

--- packages/nlu/src/nlu.ts
+++ packages/nlu/src/nlu.ts
@@ -190,6 +190,7 @@ class Nlu extends Clonable {
   async prepareCorpus(srcInput) {
     this.features = {};
     this.intents = {};
+    this.intentsArr = undefined;
     this.intentFeatures = {};
     const input = srcInput;
     const { corpus } = input;
